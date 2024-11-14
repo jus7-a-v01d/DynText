6edaa0c0425b4b43cc2d06e87ff13f54c80b4338
Tessa Todorowski <tessa.todorowski@tu-dresden.de>
2024-11-14 14:20:41 +0100

    WIP: get label to show the correct text

--- extension.js
+++ extension.js
@@ -1,29 +1,59 @@
-const {St, Clutter} = imports.gi;
+const { St, Clutter } = imports.gi;
 const Main = imports.ui.main;
+const Gio = imports.gi.Gio;
 
-TEXT = "Unlimited widgets, but no text"
+let TextStore = Gio.File.new_for_path("./show_this.txt");
+let panelButton;
 
-const Indicator = GObject.registerClass(
-    class Indicator extends PanelMenu.Button {
-        init() {
-            super.init(0.0, 'Toggle Button');
+function read_text_from_file() {
+    let text = " Could not read file :( ";
 
-            this.label = new St.Label({
-                text: TEXT,
-                y_align: Clutter.ActorAlign.CENTER,
-                style_class: 'panel-button'
-            })
+    try {
+        // read file content
+        let [success, content] = TextStore.load_contents(null);
+
+        // add spaces before and after text, for prettier padding in button
+        if (!success) {
+            text = " Hello, World! ";
+        } else {
+            //text = " " + content.toString().trim() + " ";
+            text = " AAAAA "
         }
-
-    });
-
-    enable () {
-        // create text widget
-        PanelText = new St.({
-            style_class : "panel"
-        })
+    } catch (e) {
+        log("Error reading file show_this.txt: " + e);
     }
 
-    disable() {}
+    return text;
 }
 
+function label() {
+    let text = read_text_from_file();
+
+    return new St.Label({
+        text: text,
+        y_align: Clutter.ActorAlign.CENTER,
+    });
+}
+
+function init() {
+    // Create a Button with "Hello World" text
+    panelButton = new St.Bin({
+        style_class: "panel-button",
+    });
+
+    // get and set text
+    panelButton.set_child(label());
+}
+
+function enable() {
+    // check for changes of text
+    panelButton.set_child(label());
+
+    // Add the button to the panel
+    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+}
+
+function disable() {
+    // Remove the added button from panel
+    Main.panel._rightBox.remove_child(panelButton);
+}
