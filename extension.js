8f679a8a2d02899da53e202fbd2cc6ad8b0d0d82
teto519f <tessa.todorowski@tu-dresden.de>
2025-09-04 12:43:24 +0200

    update to gnome shell version 48

--- extension.js
+++ extension.js
@@ -1,126 +1,117 @@
-const { St, Gio, Clutter, GObject, GLib } = imports.gi;
-const Mainloop = imports.mainloop;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Me = imports.misc.extensionUtils.getCurrentExtension();
+import Clutter from 'gi://Clutter';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import GObject from 'gi://GObject';
+import St from 'gi://St';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
-
-let myPopup;
-let ButtonLabel;
-let PopupItem;
-
-let timeout;
-let timer_sec;
+const CONFIG_FILENAME = '.dyntext_config.txt';
 
 const MyPopup = GObject.registerClass(
     class MyPopup extends PanelMenu.Button {
-
         _init() {
             super._init(0);
 
-            // get and set text for button
-            const texts = get_text();
+            const [label_text, menu_text] = get_text();
 
-            let label_text = texts[0];
-            let menu_text = texts[1];
-
-            ButtonLabel = new St.Label({
+            this._buttonLabel = new St.Label({
                 text: label_text,
                 y_align: Clutter.ActorAlign.CENTER,
             });
 
-            PopupItem = new PopupMenu.PopupMenuItem(
+            this._popupItem = new PopupMenu.PopupMenuItem(
                 menu_text,
                 {
                     reactive: false,
                     style_class: 'PopupMenuFont',
                 },
-            )
+            );
 
-            // add button to top bar
-            this.add_child(ButtonLabel);
+            this.add_child(this._buttonLabel);
+            this.menu.addMenuItem(this._popupItem);
+        }
 
-
-            // add menu to button
-            this.menu.addMenuItem(PopupItem);
+        updateTexts() {
+            const [label_text, menu_text] = get_text();
+            this._buttonLabel.text = label_text;
+            this._popupItem.label.text = menu_text;
         }
     });
 
-function init() {
-}
-
-function enable() {
-    myPopup = new MyPopup();
-
-    // set timer, update label and display
-    timer_sec = 30;
-    update_label();
-    Main.panel.addToStatusArea('myPopup', myPopup, 1);
-}
-
-function disable() {
-    myPopup.destroy();
-
-    // remove mainloop if it was set
-    if (timeout) {
-        Mainloop.source_remove(timeout);
-        timeout = null;
+export default class DynTextExtension {
+    constructor() {
+        this._myPopup = null;
+        this._timeout = null;
+        this._timer_sec = 30;
     }
-}
 
-function update_label() {
-    log("[DynText] Updating label...");
-    const texts = get_text();
+    enable() {
+        this._myPopup = new MyPopup();
+        this._update_label();
+        Main.panel.addToStatusArea('myPopup', this._myPopup, 1);
+    }
 
-    let label_text = texts[0];
-    let menu_text = texts[1];
+    disable() {
+        if (this._myPopup) {
+            this._myPopup.destroy();
+            this._myPopup = null;
+        }
+        if (this._timeout) {
+            GLib.source_remove(this._timeout);
+            this._timeout = null;
+        }
+    }
 
-    ButtonLabel.text = label_text;
-    PopupItem.label.text = menu_text;
+    _update_label() {
+        if (this._myPopup)
+            this._myPopup.updateTexts();
 
-    // setup mainloop with interval of timer_sec
-    if (timer_sec > 0) {
-        timeout = Mainloop.timeout_add_seconds(timer_sec, update_label);
-    } else {
-        Main.notify("DynText hint", "No timer set or timer invalid.");
-        Main.notify("Timer: " + timer_sec.toString());
-        //exit
+        if (this._timer_sec > 0) {
+            this._timeout = GLib.timeout_add_seconds(
+                GLib.PRIORITY_DEFAULT,
+                this._timer_sec,
+                () => {
+                    this._update_label();
+                    return GLib.SOURCE_CONTINUE;
+                }
+            );
+        }
     }
 }
 
 function get_text() {
-    let TextStore = Gio.File.new_for_path(".dyntext_config.txt");
+    // Use the user's home directory for the config file
+    let homeDir = GLib.get_home_dir();
+    let configPath = GLib.build_filenamev([homeDir, CONFIG_FILENAME]);
+    let TextStore = Gio.File.new_for_path(configPath);
+
     let title = " Could not read file '.dyntext_config.txt' ";
     let text = "Nothing to see here.";
 
     try {
-        // read file content
         let [success, content] = TextStore.load_contents(null);
+        let contentStr = content.toString().trimEnd();
 
-        // check for multiple newlines
-        let content_newline = content.toString().trimEnd();
-        if (content_newline.includes("\n")) {
-            let lines = content.toString().split('\n');
-
-            // parse content
+        if (contentStr.includes("\n")) {
+            let lines = contentStr.split('\n');
             title = lines[0];
-            text = lines.splice(1).join('\n');
+            text = lines.slice(1).join('\n');
         } else {
-            // add spaces before and after text, for prettier padding in button
             if (!success) {
                 title = " Hello, World! ";
             } else {
-                title = ` ${content.toString().trim()} `
-                // text = " " + content.toString().trim() + " "
+                title = ` ${contentStr.trim()} `;
             }
         }
-
-
     } catch (e) {
-        Main.notify("DynText Error", "Error reading config: " + e);
         log("Error reading config: " + e);
     }
 
     return [title, text];
+}
+
+export function init() {
+    return new DynTextExtension();
 }
