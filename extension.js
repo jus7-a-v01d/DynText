fd77fa6b5e2082d6c0cbe5a1d397bc072d0c7428
teto519f <tessa.todorowski@tu-dresden.de>
2025-09-12 12:24:10 +0200

    implement GNOME shell extension guidelines

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@ import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
@@ -14,19 +15,19 @@ const MyPopup = GObject.registerClass(
         _init() {
             super._init(0);
 
-            const [label_text, menu_text] = get_text();
+            const [labelText, menuText] = getText();
 
             this._buttonLabel = new St.Label({
-                text: label_text,
+                text: labelText,
                 y_align: Clutter.ActorAlign.CENTER,
             });
 
             this._popupItem = new PopupMenu.PopupMenuItem(
-                menu_text,
+                menuText,
                 {
                     reactive: false,
                     style_class: 'PopupMenuFont',
-                },
+                }
             );
 
             this.add_child(this._buttonLabel);
@@ -34,14 +35,16 @@ const MyPopup = GObject.registerClass(
         }
 
         updateTexts() {
-            const [label_text, menu_text] = get_text();
-            this._buttonLabel.text = label_text;
-            this._popupItem.label.text = menu_text;
+            const [labelText, menuText] = getText();
+            this._buttonLabel.text = labelText;
+            this._popupItem.label.text = menuText;
         }
     });
 
-export default class DynTextExtension {
-    constructor() {
+export default class DynTextExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
+
         this._myPopup = null;
         this._timeout = null;
         this._timer_sec = 30;
@@ -59,14 +62,16 @@ export default class DynTextExtension {
             this._myPopup = null;
         }
         if (this._timeout) {
-            GLib.source_remove(this._timeout);
+            GLib.Source.remove(this._timeout);
             this._timeout = null;
         }
+        if (this._timer_sec)
+            this._timer_sec = null;
     }
 
     _update_label() {
         if (this._timeout) {
-            GLib.source_remove(this._timeout);
+            GLib.Source.remove(this._timeout);
             this._timeout = null;
         }
 
@@ -79,39 +84,37 @@ export default class DynTextExtension {
                 this._timer_sec,
                 () => {
                     this._update_label();
-                    return true; // return true to continue, false to stop
+                    return GLib.SOURCE_CONTINUE;
                 }
             );
         }
     }
 }
 
-function get_text() {
+function getText() {
     // Use the user's home directory for the config file
     let homeDir = GLib.get_home_dir();
     let configPath = GLib.build_filenamev([homeDir, CONFIG_FILENAME]);
     let TextStore = Gio.File.new_for_path(configPath);
 
     let title = " Could not read file '.dyntext_config.txt' ";
-    let text = "Nothing to see here.";
+    let text = 'Nothing to see here.';
 
     try {
         let [success, content] = TextStore.load_contents(null);
         let contentStr = content.toString().trimEnd();
 
-        if (contentStr.includes("\n")) {
+        if (contentStr.includes('\n')) {
             let lines = contentStr.split('\n');
             title = lines[0];
             text = lines.slice(1).join('\n');
+        } else if (!success) {
+            title = ' Hello, World! ';
         } else {
-            if (!success) {
-                title = " Hello, World! ";
-            } else {
-                title = ` ${contentStr.trim()} `;
-            }
+            title = ` ${contentStr.trim()} `;
         }
     } catch (e) {
-        log("Error reading config: " + e);
+        log(`Error reading DynText config: ${e}`);
     }
 
     return [title, text];
--- readme.md
+++ readme.md
@@ -3,14 +3,14 @@ A customizable text-widget for your top bar.
 
 ## What makes it special?
 - There is **no GUI**.
-    - The text can be modified through a terminal or a text editor of your choice.
+    - The text can be modified through your terminal or a text editor of your choice.
 - Display anything you can write in **one line** of a file on your top bar
     - The only size limitation is the length of your top bar.
 - Display anything larger than one line in a **popup-menu**
     - Anything written after the first line will be put in a popup menu, activated by pressing the widget.
 - Periodic **updates**
     - Every 30 seconds the DynText widget will check your text-source again and update itself.
-    > The duration between each update will be customizable in the future.
+    > The duration between each update will be customizable in the future™.
 
 
 ## Installation
@@ -35,13 +35,23 @@ gnome-extensions disable dyntext@azubi
     echo "This text will be shown!" > ~/.dyntext_config.txt
     ```
 
+    Result:
+    ```
+    --------------------------
+    |This text will be shown!|       <- Button
+    --------------------------
+    -------------------------------
+    | Nothing to see here.        |   <- Popup, with default content
+    -------------------------------
+    ```
+
 ### Popup menu
 - append text to your config file
     ```bash
     echo "This will\nshow up\nas multiple\nlines!" >> ~/.dyntext_config.txt
     ```
 
-    This will then result in a layout like this:
+    Result:
     ```
     -----------
     |This will|       <- Button
@@ -55,5 +65,5 @@ gnome-extensions disable dyntext@azubi
 
 
 ## If this is not what you're looking for...
-This extension was inspired by the following repositories, maybe you will find something there:
+This extension was inspired by the following repository, maybe you will find something there:
 - https://github.com/Guleri24/show-me-the-text.git
